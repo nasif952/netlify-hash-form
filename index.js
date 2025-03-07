a8825702102e927fc7124deaa39e51772d0ce5e1
nasif952 <nasif.ahmed95@gmail.com>
2025-03-07 14:19:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto-js');
-const path = require('path');
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.post('/hash', (req, res) => {
     const { userid, password } = req.body;
-
     if (!userid || !password) {
         return res.status(400).send('User ID and password are required.');
     }
-
     const hashedPassword = crypto.SHA256(password).toString(crypto.enc.Hex);
-
     res.json({
-        userid: userid, // Return the plain userid
+        userid: userid,
         hashedPassword: hashedPassword,
     });
-});
-
-// Add this route to serve success.html
-app.get('/success', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'success.html'));
 });
 
 const port = process.env.PORT || 3000;
